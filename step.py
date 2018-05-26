5d6db1fc07458e24c75c86ca162725f0651c7829
猪野麻巳子 <p@iinomamiko-no-MacBook-Pro.local>
2018-05-26 15:03:41 +0900

    Create ver3

--- step.py
+++ step.py
@@ -1,4 +1,5 @@
 Ans=[]
+point=[]
 l=list(input("小文字で入力してください"))
 i=0
 while i<len(l):
@@ -25,12 +26,41 @@ for line in dictionaryWords:
         m+=1
 
     A=""
+    pt=0
     for K in k:
         K.lower
         A+=K
+        if K=="j" or K=="k" or K=="qu" or K=="x" or K=="z":
+            pt+=3
+        elif K=="c" or K=="f" or K=="h" or K=="l" or K=="m" or K=="p" or K=="v" or K=="w" or K=="y":
+            pt+=2
+        else:
+            pt+=1
+        
     k.sort()
 
-    if k==l:
-        Ans.append(A)
+    li=[]
+    for lis in l:
+        li.append(lis)
+    x=0
+    for moji in k:
+        if moji in li:
+            if moji==k[len(k)-1]:
+                Ans.append(A)
+                point.append(pt)
+            else:
+                li.remove(moji)
+        else:
+            break
 
-print(Ans)
+
+dic={}
+y=0
+while y<len(Ans):
+    dic[Ans[y]]=point[y]
+    y+=1
+
+sorted_dic=sorted(dic.items(),key=lambda z:z[1], reverse=True)
+
+for z,v in sorted_dic:
+    print(z,v)
