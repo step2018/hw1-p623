2508e07bdd4332e28f9041ba789a0536e7a9cf5a
猪野麻巳子 <p@iinomamiko-no-MacBook-Pro.local>
2018-05-26 13:04:00 +0900

    Create ver2

--- step.py
+++ step.py
@@ -1,11 +1,29 @@
 Ans=[]
 l=list(input("小文字で入力してください"))
+i=0
+while i<len(l):
+    if l[i]=="q":
+        l.remove(l[i+1])
+        l[i]="qu"
+    else:
+        pass
+    i+=1
 l.sort()
+print(l)
 
 dictionaryWords=open("dictionary.words.txt","r")
 for line in dictionaryWords:
     k=list(line)
     del k[len(k)-1]
+    m=0
+    while m<len(k):
+        if k[m]=="q":
+            k.remove(k[m+1])
+            k[m]="qu"
+        else:
+            pass
+        m+=1
+
     A=""
     for K in k:
         K.lower
